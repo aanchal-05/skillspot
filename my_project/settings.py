f35b126147bd06592c75237cff105d38df0e695d
aanchal-05 <aanchalmanchanda04@gmail.com>
2024-02-25 13:19:54 +0530

    added vercel file

--- my_project/settings.py
+++ my_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-x02@%juk)m7gvp3jw!owtrj%820y-ktp&!6ed(s064-n58fu+2
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
